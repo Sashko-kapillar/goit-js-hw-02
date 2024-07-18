8ae9411704e228df30394eb081cb834c1a7f1ad6
sashko-kapillar <kapillarua@gmail.com>
2024-07-18 15:00:23 +0300

    finished task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,11 +1,35 @@
 "use strict"; // "режим суворої сумісності"
 
-// function getShippingCost(country) {
-//   switch {
-
-//   }
-// return `Shipping to ${country} will cost ${price} credits`
-// }
+function getShippingCost(country) {
+  let price;
+  switch (country) {
+    case "China":
+      price = 100;
+      return `Shipping to ${country} will cost ${price} credits`;
+      break;
+    case "Chile":
+      price = 250;
+      return `Shipping to ${country} will cost ${price} credits`;
+      break;
+    case "Australia":
+      price = 170;
+      return `Shipping to ${country} will cost ${price} credits`;
+      break;
+    case "Jamaica":
+      price = 120;
+      return `Shipping to ${country} will cost ${price} credits`;
+      break;
+    // ...
+    default:
+      return "Sorry, there is no delivery to your country";
+  }
+}
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
 
 // function calculateTotal(number) {
 //   let sumNumber = 0;
@@ -43,13 +67,22 @@
 // }
 // console.log(calculateEvenTotal(18));
 
-const start = 6;
-const end = 17;
-let number;
-for (let i = start; i <= end; i++) {
-  if (i % 5 === 0) {
-    number = i;
-    break;
-  }
-}
+// const start = 6;
+// const end = 17;
+// let number;
+// for (let i = start; i <= end; i++) {
+//   if (i % 5 === 0) {
+//     number = i;
+//     break;
+//   }
+// }
 console.log(number);
+
+// function findNumber(start, end, divisor) {
+//   for (let i = start; i <= end; i++) {
+//     if (i % divisor === 0) {
+//       return i;
+//     }
+//   }
+// }
+// console.log(findNumber(16, 35, 7));
