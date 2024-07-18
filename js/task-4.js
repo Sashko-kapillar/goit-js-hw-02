b4ff09f1edcbe1e987b33b9d1b90e98ceb89a919
sashko-kapillar <kapillarua@gmail.com>
2024-07-18 08:29:37 +0300

    longread

--- js/task-4.js
+++ js/task-4.js
@@ -1,7 +1,25 @@
-'use strict'; // "режим суворої сумісності"
+"use strict"; // "режим суворої сумісності"
 
 // function getShippingCost(country) {
 //   switch {
-    
+
 //   }
 // }
+
+function calculateTotal(number) {
+  let sumNumber = 0;
+  while (number > 0) {
+    sumNumber = sumNumber + number;
+    number -= 1;
+  }
+  return sumNumber;
+}
+
+console.log(calculateTotal(24));
+
+// function calculateTotal(number) {
+//   for (let i = 0; number >= 0; i = i + number) {
+//     number += 1;
+//   }
+// }
+// console.log(calculateTotal(24));
